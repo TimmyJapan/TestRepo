6504bca62b7ed3aef8efa1b163cf6e3f11f96dfa
Timmy6259 <144129012+TimmyJapan@users.noreply.github.com>
2026-02-01 08:03:53 +0900

    I hope it’s work

--- src/game/mario_actions_cutscene.c
+++ src/game/mario_actions_cutscene.c
@@ -372,6 +372,8 @@ s32 common_death_handler(struct MarioState *m, s32 animation, s32 frameToDeathWa
         level_trigger_warp(m, WARP_OP_DEATH);
     }
     m->marioBodyState->eyeState = MARIO_EYES_DEAD;
+    spawn_object(o, MODEL_EXPLOSION, bhvExplosion);
+    o->activeFlags = ACTIVE_FLAG_DEACTIVATED;
     stop_and_set_height_to_floor(m);
     return animFrame;
 }
